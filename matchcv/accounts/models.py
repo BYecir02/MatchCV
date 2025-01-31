9e940e3687cda4a93b5590fbb42f0d8993d67391
BYecir02 <mohamed-yecir.badirou@student.junia.com>
2025-02-01 00:32:28 +0100

    ok

--- matchcv/accounts/models.py
+++ matchcv/accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User  
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  
+    date_of_birth = models.DateField(null=True, blank=True)
+    gender = models.CharField(max_length=10, null=True, blank=True)  # Ex: 'Male', 'Female', 'Other'
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    address = models.CharField(max_length=255, null=True, blank=True)
+    city = models.CharField(max_length=100, null=True, blank=True)
+    postal_code = models.CharField(max_length=10, null=True, blank=True)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
