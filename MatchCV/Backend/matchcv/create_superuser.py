4ff64543aefcf255c67484929bf891d457230261
BYecir02 <mohamed-yecir.badirou@student.junia.com>
2025-04-01 08:41:45 +0200

    ok

--- MatchCV/Backend/matchcv/create_superuser.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth import get_user_model
-
-# Crée un superutilisateur (nom, email, mot de passe)
-User = get_user_model()
-User.objects.create_superuser(
-    username="yecir", 
-    email="badiirouyecir@gmail.com", 
-    password="B03111969c"  # ⚠ Remplace ceci par un mot de passe fort !
-)
--- MatchCV/frontend/src/views/Dashboard.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div>
-      <h1 class="text-2xl font-bold mb-4">Tableau de bord</h1>
-      <!-- Votre contenu ici -->
-    </div>
-  </template>
